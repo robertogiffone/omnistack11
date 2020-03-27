f3fcd0894ab6ab47233635afb4385f02644e95e1
Francisco Roberto Giffone Nobre <roberto.giffone@gmail.com>
2020-03-27 01:00:02 -0300

    Front-end da aplicação

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './global.css';
+
+//import Header from './Header'; 
+//import Logon from './pages/Logon'; 
+
+import Routes from './routes';
 
 class App extends Component {
+    
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    
+    /*const [counter, setCounter] = useState(0);
+
+    function increment()
+    {
+      setCounter(counter+1);
+    }*/
+
+    /*
+      <div>
+      <Header>
+        Contador: {counter}
+      </Header>
+      <button onClick={increment}>Incrementar</button>
       </div>
+    */
+
+    return (
+      <Routes />
     );
   }
 }
